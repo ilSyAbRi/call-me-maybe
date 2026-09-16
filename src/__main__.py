20c1d6b401577a5ab9a66830a5ca7170f35132e9
ilSyAbRi <iliasssyabri1@gmail.com>
2026-09-16 13:33:16 +0100

    save process

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,15 @@
+from src.parser import load_prompts, load_functions
+
 def main():
-    print("hello from call me maybe")
+    prompts = load_prompts("data/input/function_calling_tests.json")
+    functions = load_functions("data/input/functions_definition.json")
+    
+    for prompt in prompts:
+        print(prompt)
+    
+    for function in functions:
+        print(function)
+
 
 if __name__ == "__main__":
     main()
---
+++ src/parser.py
@@ -0,0 +1,33 @@
+import json
+from src.model import Prompt, FunctionDefinition
+
+def load_json(path: str):
+    try:
+        with open(path, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print(f"Error: file not found: {path}")
+        return None 
+
+def load_prompts(path: str):
+    data = load_json(path)
+    prompts = []
+
+    for prompt_data in data:
+        prompt = Prompt(prompt=prompt_data["prompt"])
+        prompts.append(prompt)
+    return prompts
+
+def load_functions(path: str):
+    data = load_json(path)
+    functions = []
+
+    for function_data in data:
+        function = FunctionDefinition(
+            name=function_data["name"],
+            description=function_data["description"],
+            parameters=function_data["parameters"],
+            returns=function_data["returns"],
+        )
+        functions.append(function_data)
+    return functions
